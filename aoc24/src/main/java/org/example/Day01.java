1fa22abc7e62351ea1d90d6d9c673036f9ad8a4c
tbarth98 <tobias.barth1998@gmail.com>
2024-12-01 22:58:38 +0100

    Day01 part two solution

--- aoc24/src/main/java/org/example/Day01.java
+++ aoc24/src/main/java/org/example/Day01.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class Day01 {
 
+
+
     public static Integer partOne() throws IOException {
         String regexPattern = " {3}";
         Integer count = 0;
@@ -23,6 +25,27 @@ public class Day01 {
 
         for(int i = 0; i < listA.size(); i++){
             count += Math.abs(listA.get(i) - listB.get(i));
+        }
+        return count;
+    }
+
+    public static Integer partTwo() throws IOException {
+        String regexPattern = " {3}";
+        Integer count = 0;
+        List<Integer> listA = new ArrayList<>();
+        List<Integer> listB = new ArrayList<>();
+
+        List<String> fullList = PuzzleInput.readInput("Day01.txt");
+        for(String line : fullList){
+            listA.add(Integer.parseInt(line.split(regexPattern)[0]));
+            listB.add(Integer.parseInt(line.split(regexPattern)[1]));
+        }
+
+        listA.sort(Integer::compare);
+        listB.sort(Integer::compare);
+
+        for(Integer num : listA){
+            count +=  num * (int) listB.stream().filter(num::equals).count();
         }
         return count;
     }
--- aoc24/src/main/java/org/example/Main.java
+++ aoc24/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println(Day01.partOne());
+        //System.out.println(Day01.partOne());
+        System.out.println(Day01.partTwo());
     }
 }
